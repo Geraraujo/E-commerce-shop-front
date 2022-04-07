e1d7db7d3cb238de9cbdb8495511f5056e2df514
Geraraujo <araujo.german95@gmail.com>
2022-04-07 14:13:57 -0300

    Update Thanks.jsx

--- src/pages/Thanks/Thanks.jsx
+++ src/pages/Thanks/Thanks.jsx
@@ -7,7 +7,7 @@ function Thanks() {
       <div className="bg-opacity-thanks py-5 d-flex align-items-center">
         <div className="container px-4 px-lg-5 my-5 ">
           <div className="text-start text-white">
-            <h1 className="display-2 fw-bolder">Closer to toast!</h1>
+            <h1 className="display-2 fw-bolder">Cheers!</h1>
             <p className="display-5 fs-3">You will receive your order shortly</p>
           </div>
         </div>
